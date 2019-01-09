1ecfa00029b9c1b3691163e801b2b63fafa52ea6
ToniTani <toni.tani@student.saimia.fi>
2019-01-09 10:37:12 +0200

    kirjatehtävä

---
+++ Kirjaohjelma/Book.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kirjaohjelma
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public string id;
+        public double price;
+
+        public Book()
+        {
+            //nthis.name = name;
+            // Console.WriteLine("Kissan paino on alustettu arvoksi {0} ", this.weight);
+        }
+        public Book(string title, string author, string id, double price)
+        {
+            this.title = title;
+            this.author = author;
+            this.id = id;
+            this.price = price;
+            //Console.WriteLine($"Kirjan nimi {this.title}, jonka tekijä on {this.author}, ID-numero {this.id}, hinta {this.price.ToString()}");
+        }
+        public string PrintInfo()
+        {
+            return $"Kirjan nimi {this.title}, jonka tekijä on {this.author}, ID-numero {this.id}, hinta {this.price.ToString()}";
+
+        }
+
+        public string CompareBook(Book book)
+        {
+
+            Console.WriteLine($"Nimi: {this.title} - Hinta: {this.price.ToString()}");
+            Console.WriteLine($"Nimi: {book.title} - Hinta: {book.price.ToString()}");
+
+            if (this.price > book.price)
+            {
+
+                return $"{this.title} on kalliimpi kuin {book.title} kirja";
+            }
+            else
+
+            {
+                return $"{this.title} on kalliimpi kuin {book.title} kirja";
+            }
+           
+        }
+
+    }
+}
+
+
+
+
---
+++ Kirjaohjelma/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Kirjaohjelma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjat");
+
+            Book newBook = new Book("Taisteluni",
+                            "Toni Tani",
+                            "00010",
+                              22);
+            newBook.PrintInfo();
+ 
+
+            Console.WriteLine("" + newBook.PrintInfo());
+            
+
+            Book newBook2 = new Book("Peppi Pitkätossu", "Astrid Lindgren", "00002", 19);
+            newBook2.PrintInfo();
+
+
+            Console.WriteLine("" + newBook2.PrintInfo());
+            newBook.CompareBook(newBook2);
+            Console.WriteLine(newBook.CompareBook(newBook2));
+            Console.ReadKey();
+        }
+    }
+}
