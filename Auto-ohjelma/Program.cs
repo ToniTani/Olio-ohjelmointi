b1dbaff0d5b86e677429b50f4e1746ddf8032411
ToniTani <toni.tani@student.saimia.fi>
2019-01-11 15:03:54 +0200

    Auto-ohjelma

---
+++ Auto-ohjelma/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Auto
+{
+    class Car
+    {
+        static void Main(string[] args)
+        {
+            public
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
