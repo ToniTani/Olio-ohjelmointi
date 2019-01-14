340d89a2e4e7c5eb19e892538e61790bbc7252e7
ToniTani <toni.tani@student.saimia.fi>
2019-01-14 10:52:24 +0200

    Toimiva auto-ohjelma

--- Auto-ohjelma/Program.cs
+++ Auto-ohjelma/Program.cs
@@ -2,12 +2,23 @@
 
 namespace Auto
 {
-    class Car
+    class Program
     {
         static void Main(string[] args)
         {
-            public
-            Console.WriteLine("Hello World!");
+
+            Car firstCar = new Car();
+            firstCar.AskData();
+            firstCar.ShowCarInfo();
+
+            Console.WriteLine("Anna kerroin jolla auton nopeutta lisätään ja lasketaan");
+        
+            firstCar.Accelerate(Console.ReadLine());
+            firstCar.Brake();
+
+            Car secondCar = new Car();
+            secondCar.AskData();
+            secondCar.ShowCarInfo();
         }
     }
 }
