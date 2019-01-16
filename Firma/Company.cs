e7910cf6bdd854ea61cba8230b02a0089a8d82c8
ToniTani <toni.tani@student.saimia.fi>
2019-01-16 22:26:32 +0200

    Firma-luokka

---
+++ Firma/Company.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Firma
+{
+    class Company
+    {
+        //Fields
+        public string title;
+        public string adress;
+        public string phone;
+        private double income;
+        private double expense;
+
+        //Constructor 
+
+        public Company()
+        {
+
+            this.title = "unknown";
+            this.adress = "unknown";
+            this.phone = "";
+            this.income = 0;
+            this.expense = 0;
+
+        }
+
+
+        public Company(string title, string adress, string phone, double income, double expense)
+        {
+
+            this.title = title;
+            this.adress = adress;
+            this.phone = phone;
+            this.income = income;
+            this.expense = expense;
+            //Copy Constructor
+
+
+        }
+        public Company(Company previousCompany)
+        {
+            this.title = previousCompany.title;
+            this.adress = previousCompany.adress;
+            this.phone = previousCompany.phone;
+            this.income = previousCompany.income;
+            this.expense = previousCompany.expense;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Yritys: {this.title}");
+            Console.WriteLine($"Paikkakunta: {this.adress}");
+            Console.WriteLine($"Puhelinnumero: {this.phone}");
+            Console.WriteLine($"Tuotto: {this.income}");
+            Console.WriteLine($"Menot: {this.expense}");
+            Console.WriteLine("--------------------------------------------------");
+        }
+            public void Profit()
+        {
+            double profit = 100 * (this.income - this.expense) / this.expense;
+
+            Console.Write($"Yrityksen {this.title} voittoprosentti on {profit}%.");
+
+            if (profit > 300)
+            {
+                Console.WriteLine( "Yrityksellä menee hyvin");
+            }
+            else if (profit < 100)
+            {
+                Console.WriteLine( "Yrityksellä menee huonosti");
+            }
+            else
+            {
+                Console.WriteLine( "Yrityksellä menee riittävän hyvin");
+            }
+            Console.WriteLine("-----------------------------------------------------");
+        }
+    }
+}
+
---
+++ Firma/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Firma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Company kalatukku = new Company("Kalatukku", " Lappeenranta", "05012444", 400, 200);
+            Company taikuri = new Company("Taikurin Hattu", "Helvetin Portti", "050666666", 666, 999);
+            Company ravintola = new Company("Kebab ja Sushi", "Sumatra", "08983938", 5645, 1000);
+
+            kalatukku.PrintInfo();
+            taikuri.PrintInfo();
+            ravintola.PrintInfo();
+
+            kalatukku.Profit();
+            taikuri.Profit();
+            ravintola.Profit();
+
+            Console.ReadLine();
+        }
+    }
+}
