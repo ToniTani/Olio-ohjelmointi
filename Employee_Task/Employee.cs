f8ca6a50f1ca4a06c440fb3269e56e21dd134f52
ToniTani <toni.tani@student.saimia.fi>
2019-01-17 13:24:31 +0200

    Toimiva looppi

---
+++ Employee_Task/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee
+{
+    class Employee
+    {
+        public string Name;
+        public int Id;
+        public string Position;
+        public int Salary;
+
+
+        public Employee(string name, int id, string position, int salary)
+        {
+            Name = name;
+            Id = id;
+            Position = position;
+            Salary = salary;
+        }
+
+        public string CompareSalary(Employee employee)
+        {
+    
+
+            if (Salary > employee.Salary)
+            {
+                return ($"Henkilön { Name } palkka on suurempi kuin henkilön { employee.Name } palkka.");
+            }
+            else if (Salary < employee.Salary)
+            {
+                return ($"Henkilön { employee.Name } palkka on suurempi kuin henkilön { Name } palkka.");
+            }
+            else
+            {
+                return ($"Henkilön { Name } palkka on sama kuin henkilön { employee.Name } palkka.");
+            }
+        }
+
+        public void PrintEmployeeInfo()
+        {
+            Console.WriteLine($"\nTyöntekijän nimi: {Name}" +
+                $"\nTyöntekijän tunnus: {Id}" +
+                $"\nTyöntekijän titteli:{Position}" +
+                $"\nTyöntekijän palkka: {Salary}€");
+        }
+    }
+}
---
+++ Employee_Task/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Employee[] employees = new Employee[4];
+            employees[0] = new Employee("Matti Meikäläinen", 15236, "Toimitusjohtaja", 17000);
+            employees[1] = new Employee("Jutta Jussila", 78758, "Myyntipäällikkö", 4304);
+            employees[2] = new Employee("Jamo Paholainen", 666999, "Kokki", 3000);
+            employees[3] = new Employee("Jimo Tutila", 1995, "Maailmanmestari", 17000);
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                employees[i].PrintEmployeeInfo();
+            }
+            employees[0].CompareSalary(employees[3]);
+            employees[1].CompareSalary(employees[2]);
+            employees[2].CompareSalary(employees[3]);
+            employees[3].CompareSalary(employees[0]);
+            
+
+
+            employees[0].PrintEmployeeInfo();
+            employees[1].PrintEmployeeInfo();
+            employees[2].PrintEmployeeInfo();
+            employees[3].PrintEmployeeInfo();
+            Console.WriteLine();
+
+
+
+            Console.WriteLine(employees[0].CompareSalary(employees[1]));
+            Console.WriteLine(employees[1].CompareSalary(employees[2]));
+            Console.WriteLine(employees[2].CompareSalary(employees[3]));
+            Console.WriteLine(employees[3].CompareSalary(employees[0]));
+
+            Console.ReadKey();
+        }
+    }
+}
+
