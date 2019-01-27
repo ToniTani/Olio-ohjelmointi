79eaa94b8180f6c4e032c120c33b49a264f78079
ToniTani <toni.tani@student.saimia.fi>
2019-01-27 14:46:12 +0200

    ticket price solution

---
+++ TicketPriceProgram/Price.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace TaskTicketPrice
+{
+    class TicketPrice
+    {
+        //Fields
+        public int isAge;
+        public bool isMtk;
+        public int NormalPrice;
+        public decimal EndPrice;
+        public int Discount;
+        public bool isSoldier;
+        public bool isStudent;
+
+        //constructor
+        public TicketPrice()
+        {
+            isAge = 0;
+            isMtk = false;
+            NormalPrice = 16;
+            EndPrice = 0;
+            isSoldier = false;
+            isStudent = false;
+            Discount = 0;
+        }
+
+        //Methods
+        public void Data()
+        {
+            Console.WriteLine("Welcome! Let's calculate your ticket price!\n");
+
+            bool isNumber;
+            do
+            {
+                Console.Write("How old are you? ");
+                string age = Console.ReadLine();
+                isNumber = int.TryParse(age, out isAge);
+            } while (isNumber != true);
+
+            isMtk = Answer("member of the MTK?");
+            isSoldier = Answer("soldier?");
+            isStudent = Answer("student?");
+        }
+        public bool Answer(string msg)
+        {
+            Console.Write($"Are you a {msg} Y/N:");
+            string ans = Console.ReadLine().ToUpper();
+
+            if (ans == "Y")
+                return true;
+            else
+                return false;
+        }
+
+        public void PriceCount()
+        {
+
+            double price = 16.00;
+
+            if (isAge < 7)
+            {
+                price = price - (price * 1);
+            }
+            else if (isAge <= 15)
+            {
+                price = price - (price * 0.5);
+            }
+            else if (isAge >= 65)
+            {
+                price = price - (price * 0.5);
+            }
+            else if (isSoldier == true)
+            {
+                price = price - (price * 0.5);
+            }
+            else
+            {
+                if (isStudent == true)
+                {
+                    price = price - (price * 0.45);
+                }
+                if (isMtk == true)
+                {
+                    price = price - (price * 0.15);
+                }
+            }
+            Console.WriteLine($"Your ticket's final price is: {price} $");
+        }
+
+    }
+}
---
+++ TicketPriceProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TaskTicketPrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            TicketPrice person1 = new TicketPrice();
+            person1.Data();
+            person1.PriceCount();
+
+            Console.ReadKey();
+        }
+    }
+}
